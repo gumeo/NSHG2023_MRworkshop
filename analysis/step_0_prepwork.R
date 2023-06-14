49a12c26fd9ebe188e8f90549cefab2917c965a0
gumeo <gumeo140688@gmail.com>
2023-06-14 15:26:01 +0000

    Removing eoffice.

--- analysis/step_0_prepwork.R
+++ analysis/step_0_prepwork.R
@@ -1,17 +1,38 @@
+############################################################
+# Code block 1: Required packages to install
+############################################################
 # These packages are on CRAN, install with install.packages
-# if you have not already installed them
-library(tidyverse)
-library(googledrive)
-library(data.table)
-library(MendelianRandomization)
-library(simex) # to run simex correction
-library(knitr) # to make nice embedded tables
-library(furrr) # to run MR PRESSO serially
-library(eoffice) # to save plots (optional)
+# if you have not already installed them. Note that if you
+# cannot install all packages, then that is fine, still try
+# to download the sumstats with the code in the second block 
+# of this file.
+packages_to_install_from_cran <- c("tidyverse",
+                         "googledrive",
+                         "data.table",
+                         "MendelianRandomization", # MR package from S. Burgess
+                         "simex", # to run simex correction
+                         "knitr", # to make nice embedded tables
+                         "furrr", # to run MR PRESSO serially
+                         "survey") # Required for gsms
+
+for(i in 1:length(packages_to_install_from_cran)) {
+  current_package <- packages_to_install_from_cran[i]
+  if(!(current_package %in% installed.packages())){
+    install.packages(current_package)
+  }
+}
+
+# Rest of the packages are not on CRAN, need to be downloaded from
+# various other sources
 
 # MR-PRESSO
 if (!require("devtools")) { install.packages("devtools") } else {}
 devtools::install_github("rondolab/MR-PRESSO")
+
+# gsmr
+# More info: https://yanglab.westlake.edu.cn/software/gsmr/#Installation
+install.packages("https://yanglab.westlake.edu.cn/software/gsmr/static/gsmr_1.1.1.tar.gz",
+                 repos=NULL,type="source")
 
 # ggforestplot is not available for all versions on CRAN
 # Get directly from github like this
@@ -22,11 +43,14 @@ install.packages("remotes")
 remotes::install_github("MRCIEU/TwoSampleMR")
 
 ############################################################
-# Download the sumstats
+# Code block 2: Download the sumstats
 ############################################################
 
 # Download function
 source("./R/utils.R")
 
 # Downloads 4 Gb (Should take a few minutes)
+# You will be prompted to allow tidyverse to use googledrive,
+# this is expected. The files are all saved in the folder
+# sumstats under the root of this project.
 download_sumstats()
